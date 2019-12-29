a798f34f830267e50db025dd7a78af993d0d8bfc
sunlulu427 <sun_lulu427@163.com>
2019-12-29 21:26:51 +0800

    .

--- 12-29/reg.py
+++ 12-29/reg.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import scipy
 from scipy import io
+from sklearn import preprocessing
+
 # 正常显示中文
 import matplotlib as mpl
 #mpl.rcParams['font.sans-serif'] = ['SimHei']
@@ -15,7 +17,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
-
+from sklearn.model_selection import validation_curve
 # 载入模型库
 # 1.线性回归
 from sklearn import linear_model
@@ -51,56 +53,38 @@ df = scipy.io.loadmat('beta_estimate.mat')
 feature = df["beta"]
 dfdata = pd.DataFrame(data=feature)
 dfdata.to_csv('/Users/sun/Desktop/python-regression/data_tr.csv', index = True)
-data = pd.read_csv('data_tr.csv', index_col = 0)
+data = pd.read_csv('/Users/sun/Desktop/python-regression/data_tr.csv', index_col = 0)
 data.head()
 t = np.arange(0, len(data) / 100.0, 0.01).reshape(-1, 1)
 print('数据的长度:', len(t))
 print('时间：', t)
 
-time = np.arange(0, 1.5 * len(data) / 100, 0.01).reshape(-1, 1)
-
 vals = data['0'].values
 print('数据', vals)
 
-# 使用一元线性回归检验数据集
-#model_LinearRegression.fit(t, vals)
-#y_pred = model_LinearRegression.predict(t);
-#plt.figure(figsize=(14, 4))
-#plt.scatter(t, vals, color='g')
-#plt.plot(t, y_pred, color='r')
-#plt.xlabel('time(0-25)')
-#plt.show()
+time = np.arange(0, 2 * len(data) /100.0, 0.01).reshape(-1, 1)
+
+# 数据集拆分
+t_train, t_test, y_train, y_test = train_test_split(t, vals, test_size=0.25,random_state=42)
+
 
 # 定义函数
 def model_fit(model):
-	model.fit(t, vals)
-	y_pred = model.predict(t)
+	model.fit(t_train, y_train)
+	y_pred = model.predict(t_test)
 	plt.figure(figsize=(14,4))
 	plt.plot(t, vals, color='g')
-	plt.plot(t, y_pred, color='r')
+	plt.plot(t_test, y_test, color='r')
 	plt.show()
 	print('Score:+++', model.score(t, vals))
 
-
-def model_pre(regr):
-	model.fit(t, vals)
+def model_pre(model):
+	model.fit(t, vals);
 	y_pred = model.predict(time)
-	plt.figure(figsize=(14,4))
+	plt.figure(figsize=(12,4))
 	plt.plot(t, vals, color='g')
 	plt.plot(time, y_pred, color='r')
 	plt.show()
-	print('Score:+++', model.score(t, vals))
+	print("score:", model.score(t, vals))
+model_pre(model_AdaBoostRegressor)
 
-model_fit(model_DecisionTreeRegressor)
-
-# 定义多元模型
-def model_function(regr):
-	pf = PolynomialFeatures(degree=2)
-	regr.fit(pf.fit_transform(t), vals)
-	x_predict = np.linspace(0, 2500, 1440)
-	x_predict_result = regr.predict(pf.transform(x_predict.reshape(x_predict.shape[0], 1)))
-	plt.figure(figsize=(14,4))
-	plt.scatter(t, vals, color='g')
-	plt.plot(x_predict,x_predict_result, 'r-')
-	plt.xlabel('time')
-	plt.show()
